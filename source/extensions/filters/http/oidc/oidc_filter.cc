27fc7a8a8b503b456fe7dfeee52200f54a05e7af
Nick A. Smith <nick.a.smith@thalesesecurity.com>
2019-03-21 12:40:44 +0000

    Fixing broken CI builds
    - Added missing override directives
    - Added missing ssl library requirement to state_store_lib
    - Removed unused static variable from OIDC filer
    
    Signed-off-by: Nick A. Smith <nick.a.smith@thalesesecurity.com>

--- source/extensions/filters/http/oidc/oidc_filter.cc
+++ source/extensions/filters/http/oidc/oidc_filter.cc
@@ -30,8 +30,6 @@ namespace Oidc {
 namespace {
 
 const char* nonceClaim = "nonce";
-const char hexTable[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
-                           '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 const std::vector<LowerCaseString> validTokenResponseContentTypes = {
     LowerCaseString{"application/json"},
     LowerCaseString{"application/json;charset=utf-8"},
